f8704758debe7dc64ea3ba18c1ed091880f2e6ed
Maciej Marchel <maciejmarchel14@gmail.com>
2023-12-19 16:46:47 +0000

    Mongo lab Exercises done

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -4,6 +4,8 @@ import tasksRouter from './api/tasks';
 import './db';
 //... other imports
 import usersRouter from './api/users';
+import cors from 'cors';
+
 
 dotenv.config();
 
@@ -17,6 +19,10 @@ const errHandler = (err, req, res, next) => {
   };
 
 const app = express();
+
+// Enable CORS for all requests
+app.use(cors());
+
 
 const port = process.env.PORT;
 app.use(express.json());
