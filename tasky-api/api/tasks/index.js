ffd8eb184c092683a3b5152cac237ea02e62fa70
Maciej Marchel <maciejmarchel14@gmail.com>
2023-12-19 15:24:03 +0000

    Week 9 Lab exercise done

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -2,8 +2,7 @@ import express from 'express';
 import { tasksData } from './tasksData';
 import { v4 as uuidv4 } from 'uuid';
 
-
-const router = express.Router(); 
+const router = express.Router();
 
 router.get('/', (req, res) => {
     res.json(tasksData);
@@ -11,7 +10,7 @@ router.get('/', (req, res) => {
 
 // Get task details
 router.get('/:id', (req, res) => {
-    const { id } = req.params
+    const { id } = req.params;
     const task = tasksData.tasks.find(task => task.id === id);
     if (!task) {
         return res.status(404).json({ status: 404, message: 'Task not found' });
@@ -19,7 +18,7 @@ router.get('/:id', (req, res) => {
     return res.status(200).json(task);
 });
 
-//Add a task
+// Add a task
 router.post('/', (req, res) => {
     const { title, description, deadline, priority, done } = req.body;
     const newTask = {
@@ -28,31 +27,39 @@ router.post('/', (req, res) => {
         description,
         deadline,
         priority,
-        done
+        done,
+        created_at: new Date().toISOString(),
+        updated_at: new Date().toISOString(),
     };
     tasksData.tasks.push(newTask);
     res.status(201).json(newTask);
     tasksData.total_results++;
 });
 
-//Update an existing task
+// Update an existing task
 router.put('/:id', (req, res) => {
     const { id } = req.params;
-    const taskIndex = tasksData.tasks.findIndex(task => task.id === id); 
+    const taskIndex = tasksData.tasks.findIndex(task => task.id === id);
     if (taskIndex === -1) {
         return res.status(404).json({ status: 404, message: 'Task not found' });
     }
-    const updatedTask = { ...tasksData.tasks[taskIndex], ...req.body, id:id };
+    const updatedTask = {
+        ...tasksData.tasks[taskIndex],
+        ...req.body,
+        updated_at: new Date().toISOString(),
+        id: id,
+    };
     tasksData.tasks[taskIndex] = updatedTask;
     res.json(updatedTask);
 });
 
-//Delete a task
+// Delete a task
 router.delete('/:id', (req, res) => {
     const { id } = req.params;
     const taskIndex = tasksData.tasks.findIndex(task => task.id === id);
-    
-    if (taskIndex === -1) return res.status(404).json({status:404,message:'Task not found'});
+
+    if (taskIndex === -1)
+        return res.status(404).json({ status: 404, message: 'Task not found' });
     tasksData.tasks.splice(taskIndex, 1);
     res.status(204).send();
     tasksData.total_results--;
