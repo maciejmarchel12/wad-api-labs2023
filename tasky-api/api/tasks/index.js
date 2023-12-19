69e6b54b2ca9595e5635c53d7d737d8259eecb0e
Maciej Marchel <maciejmarchel14@gmail.com>
2023-12-19 16:12:00 +0000

    Mongo lab Users Router

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import Task from './taskModel';
+import asyncHandler from 'express-async-handler';
+
 
 const router = express.Router(); // eslint-disable-line
 
 // Get all tasks
 router.get('/', async (req, res) => {
-    const tasks = await Task.find();
+    const tasks = await Task.find().populate('userId', 'username');
     res.status(200).json(tasks);
 });
 
--- tasky-api/index.js
+++ tasky-api/index.js
@@ -2,7 +2,8 @@ import dotenv from 'dotenv';
 import express from 'express';
 import tasksRouter from './api/tasks';
 import './db';
-
+//... other imports
+import usersRouter from './api/users';
 
 dotenv.config();
 
@@ -23,6 +24,9 @@ app.use(express.json());
 app.use('/api/tasks', tasksRouter);
 
 app.use(errHandler);
+
+//Users router
+app.use('/api/users', usersRouter);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
