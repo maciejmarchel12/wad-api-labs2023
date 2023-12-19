c9b8c44163b33beaa698977a5385927977ecd68a
Maciej Marchel <maciejmarchel14@gmail.com>
2023-12-19 19:28:08 +0000

    Added JWT

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import User from './userModel';
 import asyncHandler from 'express-async-handler';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
@@ -18,27 +19,19 @@ router.get('/', async (req, res) => {
 
 // register(Create)/Authenticate User
 router.post('/', asyncHandler(async (req, res) => {
-    if (req.query.action === 'register') {
-        // Validate password before saving to DB
-        if (!isPasswordValid(req.body.password)) {
-            return res.status(400).json({
-                code: 400,
-                msg: 'Password must be at least 8 characters long and contain at least one letter, one digit, and one special character.',
-            });
+    try {
+        if (!req.body.username || !req.body.password) {
+            return res.status(400).json({ success: false, msg: 'Username and password are required.' });
         }
-
-        await User(req.body).save();
-        res.status(201).json({
-            code: 201,
-            msg: 'Successfully created a new user.',
-        });
-    } else {
-        const user = await User.findOne(req.body);
-        if (!user) {
-            return res.status(401).json({ code: 401, msg: 'Authentication failed' });
+        if (req.query.action === 'register') {
+            await registerUser(req, res);
         } else {
-            return res.status(200).json({ code: 200, msg: 'Authentication Successful', token: 'TEMPORARY_TOKEN' });
+            await authenticateUser(req, res);
         }
+    } catch (error) {
+        // Log the error and return a generic error message
+        console.error(error);
+        res.status(500).json({ success: false, msg: 'Internal server error.' });
     }
 }));
 
@@ -64,5 +57,26 @@ router.put('/:id', async (req, res) => {
         res.status(404).json({ code: 404, msg: 'Unable to Update User' });
     }
 });
+
+async function registerUser(req, res) {
+    // Add input validation logic here
+    await User.create(req.body);
+    res.status(201).json({ success: true, msg: 'User successfully created.' });
+}
+
+async function authenticateUser(req, res) {
+    const user = await User.findByUserName(req.body.username);
+    if (!user) {
+        return res.status(401).json({ success: false, msg: 'Authentication failed. User not found.' });
+    }
+
+    const isMatch = await user.comparePassword(req.body.password);
+    if (isMatch) {
+        const token = jwt.sign({ username: user.username }, process.env.SECRET);
+        res.status(200).json({ success: true, token: 'BEARER ' + token });
+    } else {
+        res.status(401).json({ success: false, msg: 'Wrong password.' });
+    }
+}
 
 export default router;
